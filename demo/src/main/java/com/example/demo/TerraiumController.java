17b31bccd6d1bc66cee065d66e3b81d36cd7758a
shaqianqian <sqq940814@gmail.com>
2018-11-07 23:29:41 +0100

    modifie

--- demo/src/main/java/com/example/demo/TerraiumController.java
+++ demo/src/main/java/com/example/demo/TerraiumController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
