dd52aae3365e09fed0807dcab4f97c155f65f962
shaqianqian <sqq940814@gmail.com>
2018-11-07 22:38:45 +0100

    sha

---
+++ demo/src/main/java/com/example/demo/TerraiumController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class TerraiumController {
+
+    /**
+     * recuperer tous les parametres de terraium
+     * @return
+     */
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+
+    public String hello() {
+      return "hello";
+
+    }
+
+}
+
+
