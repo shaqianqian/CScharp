2a7a729176bb3c5e2ecb1790f553d216abffd1e4
shaqianqian <sqq940814@gmail.com>
2018-11-10 19:30:25 +0100

    fini

--- oti_tp1/src/fr/ulille1/fil/odeva/MoneyOps.java
+++ oti_tp1/src/fr/ulille1/fil/odeva/MoneyOps.java
@@ -18,6 +18,9 @@ public class MoneyOps {
   public static Money simpleAdd(Money m1, Money m2) throws IncompatibleCurrencyException, UnexistingCurrencyException
   {
     Money m=mf.createMoney(m1.getValue()+m2.getValue(),m1.getCurrency());
+    if (!m1.getCurrency().equals(m2.getCurrency())) {
+      throw new IncompatibleCurrencyException(m1.getCurrency(),m2.getCurrency());
+    }
     return m;
   }
 }
