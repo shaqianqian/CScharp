df5948500434012a72f131f02a7f2c428dc23d16
shaqianqian <sqq940814@gmail.com>
2019-01-30 11:02:43 +0100

    create init

---
+++ aspnet-master/WebApplication1/WebApplication1/Controllers/HelloworldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        // GET: Helloworld
+        public ActionResult Welcome(String name, int numTimes=1)
+        {
+            ViewBag.Name = name;
+            ViewBag.Num = numTimes;
+            return View();
+        }
+    }
+}
---
+++ aspnet-master/WebApplication1/WebApplication1/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    using System;
+
+    namespace MvcMovie.Models
+    {
+        public class Movie
+        {
+            public int ID { get; set; }
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public decimal Price { get; set; }
+            public Movie(int id, string title,string genre, decimal price) {
+                ID = id;
+                Title = title;
+                Genre = genre;
+                Price = price;
+
+            }
+            public Movie()
+            {
+           
+
+            }
+        }
+    }
+ 
+}
